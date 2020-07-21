4f463382036dfdb2b5156f1705ec6880cb69c148
Emerson MX <emersonmx@gmail.com>
2020-07-20 21:07:24 -0300

    Add config path and tests

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
+func GetConfigDir() string {
+	configPath := os.Getenv("STM_CONFIG_PATH")
+	if configPath != "" {
+		return configPath
+	}
+
+	configHome := os.Getenv("XDG_CONFIG_HOME")
+	if configHome == "" {
+		configHome = path.Join(os.Getenv("HOME"), ".config")
+	}
+
+	return path.Join(configHome, "stm")
+}
+
+func SetupConfigPath() {
+	configPath := GetConfigDir()
+	os.Setenv("STM_CONFIG_PATH", configPath)
+	os.MkdirAll(configPath, 0755)
+}
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println(GetConfigDir())
 }
