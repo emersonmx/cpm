b7f80bcfb0e19613286c23c6fc9b4571c885dee2
Emerson MX <emersonmx@gmail.com>
2020-08-15 06:01:46 -0300

    Finish stm 😁

--- main.go
+++ main.go
@@ -1,31 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"path"
-)
-
-func GetConfigDir() string {
-	configPath := os.Getenv("STM_CONFIG_PATH")
-	if configPath != "" {
-		return configPath
-	}
-
-	configHome := os.Getenv("XDG_CONFIG_HOME")
-	if configHome == "" {
-		configHome = path.Join(os.Getenv("HOME"), ".config")
-	}
-
-	return path.Join(configHome, "stm")
-}
-
-func SetupConfigPath() {
-	configPath := GetConfigDir()
-	os.Setenv("STM_CONFIG_PATH", configPath)
-	os.MkdirAll(configPath, 0755)
-}
+import "github.com/emersonmx/stm/cmd"
 
 func main() {
-	fmt.Println(GetConfigDir())
+	cmd.Execute()
 }
